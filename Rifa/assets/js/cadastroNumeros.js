f11b5f646b9ee1d21f2e9395a82956980f7a4f78
JosemirAndrade <114767293+JosemirAndrade@users.noreply.github.com>
2022-09-30 16:13:15 -0300

    Add files via upload

---
+++ Rifa/assets/js/cadastroNumeros.js
@@ -0,0 +1,73 @@
+let cadastroNumeros = [
+  
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+    {
+        numero: '',
+        situacao: '',
+        participante: '',
+        clicado: true,
+        telefone: ''
+    },
+];
