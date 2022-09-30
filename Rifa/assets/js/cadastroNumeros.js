ea69dabe3b13ddfb0a92bc1514efd847ce6bf70c
JosemirAndrade <114767293+JosemirAndrade@users.noreply.github.com>
2022-09-30 16:29:05 -0300

    Update cadastroNumeros.js

--- Rifa/assets/js/cadastroNumeros.js
+++ Rifa/assets/js/cadastroNumeros.js
@@ -1,67 +1,129 @@
 let cadastroNumeros = [
-  
-    {
-        numero: '',
-        situacao: '',
-        participante: '',
+   {
+        numero: '0001',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
         clicado: true,
-        telefone: ''
+        telefone: '(88) 98110-8634'
     },
     {
-        numero: '',
-        situacao: '',
-        participante: '',
+        numero: '0002',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
         clicado: true,
-        telefone: ''
+        telefone: '(88) 98110-8634'
     },
     {
-        numero: '',
-        situacao: '',
-        participante: '',
+        numero: '0003',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
         clicado: true,
-        telefone: ''
+        telefone: '(88) 98110-8634'
     },
     {
-        numero: '',
-        situacao: '',
-        participante: '',
+        numero: '0004',
+        situacao: 'Pago',
+        participante: 'Erisvando Gonçalves',
         clicado: true,
-        telefone: ''
+        telefone: '(88) 99907-6037'
     },
     {
-        numero: '',
-        situacao: '',
-        participante: '',
+        numero: '0005',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
         clicado: true,
-        telefone: ''
+        telefone: '(88) 98110-8634'
     },
     {
-        numero: '',
-        situacao: '',
-        participante: '',
+        numero: '0006',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
         clicado: true,
-        telefone: ''
+        telefone: '(88) 98110-8634'
     },
     {
-        numero: '',
-        situacao: '',
-        participante: '',
+        numero: '0007',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
         clicado: true,
-        telefone: ''
+        telefone: '(88) 98110-8634'
     },
     {
-        numero: '',
-        situacao: '',
-        participante: '',
+        numero: '0008',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
         clicado: true,
-        telefone: ''
+        telefone: '(88) 98110-8634'
     },
     {
-        numero: '',
-        situacao: '',
-        participante: '',
+        numero: '0009',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
         clicado: true,
-        telefone: ''
+        telefone: '(88) 98110-8634'
+    },
+    {
+        numero: '0010',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
+        clicado: true,
+        telefone: '(88) 98110-8634'
+    },
+    {
+        numero: '0011',
+        situacao: 'Pago',
+        participante: 'Rafaela Brena Ferreira Vasconcelos',
+        clicado: true,
+        telefone: '(88) 8177-0331'
+    },
+    {
+        numero: '0012',
+        situacao: 'Pago',
+        participante: 'Ariana pereira dos Santos',
+        clicado: true,
+        telefone: '(88) 99283-1510'
+    },
+    {
+        numero: '0013',
+        situacao: 'Pago',
+        participante: 'Paulo de Assis Melo',
+        clicado: true,
+        telefone: '(88) 98110-8634'
+    },
+    {
+        numero: '0014',
+        situacao: 'Pago',
+        participante: 'Washington Wender Rodrigues de Oliveira',
+        clicado: true,
+        telefone: '(85) 99747-8963'
+    },
+    {
+        numero: '0015',
+        situacao: 'Pago',
+        participante: 'Quileana Batista de Oliveira',
+        clicado: true,
+        telefone: '(88) 98886-6457'
+    },
+    {
+        numero: '0016',
+        situacao: 'Pago',
+        participante: 'Ariana pereira dos Santos',
+        clicado: true,
+        telefone: '(88) 99283-1510'
+    },
+    {
+        numero: '0017',
+        situacao: 'Pago',
+        participante: 'Richarle Gonçalves',
+        clicado: true,
+        telefone: '(88) 98818-8382'
+    },
+    {
+        numero: '0018',
+        situacao: 'Pago',
+        participante: 'Ariana pereira dos Santos',
+        clicado: true,
+        telefone: '(88) 99283-1510'
     },
     {
         numero: '',
