77ecc590b0cc2d0c042b9e2bd3189b261a94e387
JosemirAndrade <114767293+JosemirAndrade@users.noreply.github.com>
2022-10-01 17:46:55 -0300

    Update cadastroNumeros.js

--- Rifa/assets/js/cadastroNumeros.js
+++ Rifa/assets/js/cadastroNumeros.js
@@ -45,512 +45,439 @@ let cadastroNumeros = [
         numero: '08- Conjunto de peneiras',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
     {
         numero: '09- Cesta de pão',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
     {
         numero: '10- Kit tapete pra banheiro',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
     {
         numero: '11- Jogo americano',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
     {
         numero: '12- Baldes plásticos (2un)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '13- Lixeira pra pia e banheiro',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '14- Ralador',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '15- Conjunto de conchas',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '16- Pano de prato (4un)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '17- Cesto de roupas',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '18- Kit Toalha de mão',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '19- Tábua de corte',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '20- Toalha de rosto (4un)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '21- Jogo de xícaras',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '22- A ESCOLHA',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '23- Potes Plásticos Multiuso (conj)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '24- Saladeira',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '25- Toalha de banho (2un)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '26- Conjunto de bandejas',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '27- Potes pra microndas e freezer',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '28- Kit tapete de cozinha',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '29- A ESCOLHA',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '30- Escorredor de pratos',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '31- Bacias plásticas pequenas (conj)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '32- Conjunto de formas',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '33- Cuscuzeira',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '34- Potes plásticos multiuso (conj)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '35- Formas de pudim',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '36- A ESCOLHA',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '37- Frigideira pequena',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '38- Conjunto de canecas',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '39- Garrafa de café',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '40- A ESCOLHA',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '41- Jogo de lençol casal',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '42- Fruteira',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '43- Chaleira',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '44- Toalha de mesa (conj)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '45- Kit sobremesa',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '46- Conjunto de taças',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '47- Travessa de vidro pequena',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '48- A ESCOLHA',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '49- Pratos de sobremesa',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '50- Panela grande (conj)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '51- A ESCOLHA',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '52- Tapete (conj)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '53- Frigideira grande',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '54- Conjunto de pratos rasos',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '55- Leiteira',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '56- Jarra de suco',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '57- Conjunto de copos',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '58- Faqueiro',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '59- A ESCOLHA',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '60- Potes Plásticos Multiuso (conj)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '61- Jarra de vidro grande',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '62- A ESCOLHA',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '63- Travessa de vidro grande',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '64- Travesseiros(2 un)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '65- Tigela(2 un)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '66- Potes de vidro',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '67- Rede',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '68- Panela pequena (conj)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '69- A ESCOLHA',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '70- Conjunto de pratos fundos',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '71- Colcha de cama (conj)',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '72- Travessa de vidro grande',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '73- Sanduicheira',
-        situacao: 'Reservado',
-        participante: 'Juliana',
-        clicado: true,
-        telefone: ''
+        situacao: 'Disponível',
+        participante: '',
+        clicado: false
     },
    {
         numero: '74- Ferro de passar roupa',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '75- Batedeira',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '76- Cafeteira',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '77- Espremedor de frutas',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '78- Tábua de passar roupa',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '79- Panela de pressão grande',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
    {
         numero: '80- Processador',
         situacao: 'Disponível',
         participante: '',
-        clicado: false,
-        telefone: ''
+        clicado: false
     },
   
 ];
