348c50d3042468a7d5f41e1b4f282092143c2354
JosemirAndrade <114767293+JosemirAndrade@users.noreply.github.com>
2022-11-29 19:28:32 -0300

    Update script.js

--- Rifa/assets/js/script.js
+++ Rifa/assets/js/script.js
@@ -57,7 +57,7 @@ criarNumeros()
 function criarNumeros() {
   cadastroNumeros.filter(item => {
     if(item.numero !== '') {
-      if(item.situacao === 'Pago') {
+      if(item.situacao === 'Ok') {
         containerNumbers.innerHTML += `<button class="button ok" id="${item.numero}" onclick="clicou('${item.numero}')">${item.numero}</button>`;
       }else if(item.situacao === 'Reservado') {
         containerNumbers.innerHTML += `<button class="button reservado" id="${item.numero}" onclick="clicou('${item.numero}')">${item.numero}</button>`;
@@ -75,8 +75,8 @@ function criarNumeros() {
   let numerosReservados = cadastroNumeros.filter(item => item.situacao === 'Reservado').length;
   nreservados.innerHTML = numerosReservados;
 
-  let numerosPagos = cadastroNumeros.filter(item => item.situacao === 'Pago').length;
-  npagos.innerHTML = numerosPagos;
+  let numerosOk = cadastroNumeros.filter(item => item.situacao === 'Ok').length;
+  nok.innerHTML = numerosOk;
 }
 
 function clicou(n) {
@@ -87,7 +87,7 @@ function clicou(n) {
   carrinho.sort((a,b) => a- b);
 
   cadastroNumeros.filter(item => {
-    if(n === item.numero && (item.situacao === 'Reservado' || item.situacao === 'Pago')) {
+    if(n === item.numero && (item.situacao === 'Reservado' || item.situacao === 'Ok')) {
       mostrarBilhete.style.display = 'flex';
       mostrarNumero.innerHTML = `Bilhete: ${item.numero}`;
       mostrarSituacao.innerHTML = `Status: ${item.situacao}`;
